906e4f5c721cba6f6b0eb1f2556b8835ca60dbe3
Tom Kiefhaber <tom.kiefhaber@hungrymachine.com>
2014-03-29 13:06:17 -0400

    initial commit with oauth & pg setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KateRealEstate::Application.config.secret_key_base = 'fddbbb9b7bb54bd9405e13032908a0c7decca40a3671e01910c695f8a99ca0c01d4ad559ded8ab1f503e0189fd8abfcef020255c9e6fa259496ad343e9543a86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KateRealEstate::Application.config.session_store :cookie_store, key: '_kate_real_estate_session'
